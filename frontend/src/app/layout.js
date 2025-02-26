dcec88d4c1ddaa3b5fcecfef9db9fa51124414de
Azizul Islam <azizultushar98@gmail.com>
2025-02-26 23:19:23 +0600

    Initialize frontend with Next.js, Tailwind CSS, and essential configurations

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,56 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import FloatingShape from "@/components/FloatingShape";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Auth App",
+  description: "Full-Stack Authentication App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div
+          className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 
+        flex justify-center items-center relative overflow-hidden"
+        >
+          <FloatingShape
+            color="bg-green-500"
+            size="w-64 h-64"
+            top="-5%"
+            left="10%"
+            delay={0}
+          />
+          <FloatingShape
+            color="bg-emerald-500"
+            size="w-48 h-48"
+            top="70%"
+            left="80%"
+            delay={5}
+          />
+          <FloatingShape
+            color="bg-lime-500"
+            size="w-32 h-32"
+            top="40%"
+            left="-10%"
+            delay={2}
+          />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
