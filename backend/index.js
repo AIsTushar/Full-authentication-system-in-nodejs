7b739afbe085b2cf8668b78a76840320d2e41ac7
Azizul Islam <azizultushar98@gmail.com>
2025-03-17 19:42:12 +0600

    Add production build support and update start script; serve static files from frontend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cros from "cors";
+import path from "path";
 
 import { connectDB } from "./db/connect.js";
 import authRoutes from "./routes/auth.route.js";
@@ -15,6 +16,8 @@ app.use(
   })
 );
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,6 +26,14 @@ const port = process.env.PORT || 5000;
 dotenv.config();
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   connectDB();
